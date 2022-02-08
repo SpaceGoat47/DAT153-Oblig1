60e5600602be93d34ad757774b97703c48bf048a
SpaceGoat47 <henrik.tveit14@gmail.com>
2022-02-08 14:34:11 +0100

    trying to implement Animal object

--- app/src/main/java/com/example/dat153oblig1/MainActivity.java
+++ app/src/main/java/com/example/dat153oblig1/MainActivity.java
@@ -7,6 +7,8 @@ import android.content.Intent;
 import android.media.Image;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.provider.ContactsContract;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -18,32 +20,26 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private static final String TAG = "MainActivity";
     //fields
-    private Button btnDatabase;
-    private Button btnQuiz;
-    private Button btnAddEntry;
-    //to be removed
-    private Map<String, Integer> database;
-
-    private ArrayList<String> names;
-    private ArrayList<Integer> images;
-    private Intent intentDatabase, intentQuiz, intentAddEntry;
+    private Button btnDatabase, btnQuiz, btnAddEntry;
+    private ArrayList<Animal> database;
+    private Intent intent;
 
     @Override
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.btnDatabase:
-                setContentView(R.layout.activity_database);
-                startActivity(intentDatabase);
-                Toast.makeText(this, "Going to Database", Toast.LENGTH_SHORT).show();
+                //setContentView(R.layout.activity_database);
+                intent = new Intent(this, DatabaseActivity.class);
                 break;
             case R.id.btnQuiz:
-                setContentView(R.layout.activity_quiz);
-                startActivity(intentQuiz);
+                //setContentView(R.layout.activity_quiz);
+                intent = new Intent(this, QuizActivity.class);
                 break;
             case R.id.btnAddEntry:
-                setContentView(R.layout.activity_add_entry);
-                startActivity(intentAddEntry);
+                //setContentView(R.layout.activity_add_entry);
+                intent = new Intent(this, AddEntryActivity.class);
                 break;
             default:
                 break;
@@ -55,45 +51,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Initializing the Views
+        Log.d(TAG, "TEST1");
+        //Initialize database and populate it
+        Database db = new Database(database);
+        db.initializeDatabase();
+
+        Log.d(TAG, "TEST2");
+
+        //initialize buttons
         btnDatabase = findViewById(R.id.btnDatabase);
         btnQuiz = findViewById(R.id.btnQuiz);
         btnAddEntry = findViewById(R.id.btnAddEntry);
-
-        //initializing the intents
-        intentDatabase = new Intent(this, DatabaseActivity.class);
-        intentQuiz = new Intent(this, QuizActivity.class);
-        intentAddEntry = new Intent(this, AddEntryActivity.class);
-
-        //set the onClickListener
-        //this refers to View.OnClickListener
-        btnDatabase.setOnClickListener(this);
-        btnQuiz.setOnClickListener(this);
-        btnAddEntry.setOnClickListener(this);
-
-        //TO BE REMOVED
-        database = new HashMap<>();
-        database.put("Cat", R.drawable.ic_cat);
-        database.put("Dog", R.drawable.dog);
-        database.put("Horse", R.drawable.horse);
-        //
-
-        //initialize the ArrayLists and insert items
-        names = new ArrayList<>();
-        images = new ArrayList<>();
-        names.add("Dog");
-        names.add("Horse");
-        names.add("Cat");
-        images.add(R.drawable.dog);
-        images.add(R.drawable.horse);
-        images.add(R.drawable.ic_cat);
-
-        intentDatabase.putExtra("images", images);
-        intentDatabase.putExtra("names", names);
-
-        //intentAddEntry.putExtra("hashmap", (Serializable) database);
-        //intentDatabase.putExtra("hashmap", (Serializable) database);
-
 
     }
 }
